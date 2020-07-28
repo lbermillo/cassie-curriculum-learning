ab774e59129e29e0bb13f944295cf02d8e8282a8
lbermillo <lorenzo.bermillo@gmail.com>
2020-07-28 18:05:42 -0500

    Updated env and new env with phase added

--- cassie/envs/cassie_standing_phase.py
+++ cassie/envs/cassie_standing_phase.py
@@ -1,13 +1,17 @@
+import os
+import random
+from math import floor
+
 import gym
 import numpy as np
-
 from cassie.cassiemujoco import pd_in_t, state_out_t, CassieSim, CassieVis
+from cassie.trajectory import CassieTrajectory
 
 
 # Creating the Standing Environment
 class CassieEnv:
 
-    def __init__(self, simrate=60, clock_based=True, state_est=True,
+    def __init__(self, traj="walking", simrate=60, clock_based=True, state_est=True, use_phase=False,
                  reward_cutoff=0.3, target_action_weight=1.0, target_height=0.9, forces=(0, 0, 0),
                  min_height=0.4, max_height=3.0, config="cassie/cassiemujoco/cassie.xml"):
 
@@ -53,6 +57,16 @@ class CassieEnv:
         self.observation_space = np.zeros(self.observation_size)
         self.action_space = gym.spaces.Box(-1. * np.ones(10), 1. * np.ones(10))
 
+        dirname = os.path.dirname(__file__)
+        if traj == "walking":
+            traj_path = os.path.join(dirname, "..", "trajectory", "stepdata.bin")
+
+        elif traj == "stepping":
+            # traj_path = os.path.join(dirname, "trajectory", "spline_stepping_traj.pkl")
+            traj_path = os.path.join(dirname, "..", "trajectory", "more-poses-trial.bin")
+
+        self.trajectory = CassieTrajectory(traj_path)
+
         # Initial Actions
         self.P = np.array([100, 100, 88, 96, 50])
         self.D = np.array([10.0, 10.0, 8.0, 9.6, 5.0])
@@ -62,6 +76,9 @@ class CassieEnv:
         self.cassie_state = state_out_t()
         self.simrate = simrate
         self.speed = 0
+
+        self.phase = 0  # portion of the phase the robot is in
+        self.phaselen = floor(len(self.trajectory) / self.simrate) - 1
 
         # See include/cassiemujoco.h for meaning of these indices
         self.pos_idx = [7, 8, 9, 14, 20, 21, 22, 23, 28, 34]
@@ -140,13 +157,29 @@ class CassieEnv:
 
         return state, reward, done, {}
 
-    def reset(self):
-        self.sim.full_reset()
-        self.reset_cassie_state()
+    def reset(self, phase=None):
+        self.phase = int(phase) if phase is not None else random.randint(0, self.phaselen)
+
+        # get the corresponding state from the reference trajectory for the current phase
+        qpos, qvel = self.get_ref_state(self.phase)
+
+        self.sim.set_qpos(qpos)
+        self.sim.set_qvel(qvel)
 
         return self.get_full_state()
 
-    def reset_cassie_state(self):
+    def reset_cassie_state(self, pelvis=None, motor=None, joint=None):
+        if pelvis is None and motor is None and joint is None:
+            pelvis = {'position': [0, 0, 1.01],
+                      'orientation': [1, 0, 0, 0],
+                      'rotationalVelocity': np.zeros(3),
+                      'translationalVelocity': np.zeros(3),
+                      'translationalAcceleration': np.zeros(3)}
+            motor = {'position': [0.0045, 0, 0.4973, -1.1997, -1.5968, 0.0045, 0, 0.4973, -1.1997, -1.5968],
+                     'velocity': np.zeros(10)}
+            joint = {'position': [0, 1.4267, -1.5968, 0, 1.4267, -1.5968],
+                     'velocity': np.zeros(6)}
+
         # Only reset parts of cassie_state that is used in get_full_state
         self.cassie_state.pelvis.position[:] = [0, 0, 1.01]
         self.cassie_state.pelvis.orientation[:] = [1, 0, 0, 0]
@@ -159,81 +192,68 @@ class CassieEnv:
         self.cassie_state.joint.position[:] = [0, 1.4267, -1.5968, 0, 1.4267, -1.5968]
         self.cassie_state.joint.velocity[:] = np.zeros(6)
 
-    def compute_reward(self, qpos, qvel, foot_pos, foot_grf, grf_tolerance=25, rw=(0.15, 0.15, 0.15, 0.2, 0.2, 0.15, 0),
-                       multiplier=500):
-
+    def compute_reward(self, qpos, qvel, foot_pos, foot_grf, rw=(0., 0., 0.1, 0., 0., 0.2, 0.7), multiplier=5.):
         left_foot_pos = foot_pos[:3]
         right_foot_pos = foot_pos[3:]
 
         # midfoot position
         foot_pos = np.concatenate([left_foot_pos - self.midfoot_offset[:3], right_foot_pos - self.midfoot_offset[3:]])
 
-        # A. Task Rewards
-
+        # A. Standing Rewards
         # 1. Pelvis Orientation
         # r_pelvis_roll = np.exp(-qpos[4] ** 2)
         # r_pelvis_pitch = np.exp(-qpos[5] ** 2)
         # r_pelvis_yaw = np.exp(-qpos[6] ** 2)
         #
         # r_pose = 0.335 * r_pelvis_roll + 0.33 * r_pelvis_pitch + 0.335 * r_pelvis_yaw
-        # TODO: Which one should be used?
         target_pose = np.array([1, 0, 0, 0])
-        r_pose = np.exp(-100 * np.linalg.norm(qpos[3:7] - target_pose, 1) ** 2)
+        r_pose = np.exp(-multiplier * np.sum(np.abs(qpos[3:7] - target_pose)) ** 2)
 
         # 2. CoM Position Modulation
-
-        # TODO: Is this still needed? Seemed to work on some policies but clashes with other rewards
         # 2a. Horizontal Position Component (target position is the center of the support polygon)
-        xy_target_pos = np.array([0.5 * (foot_pos[0] + foot_pos[3]),
-                                  0.5 * (foot_pos[1] + foot_pos[4])])
+        xy_target_pos = np.array([0.5 * (np.abs(foot_pos[0]) + np.abs(foot_pos[3])),
+                                  0.5 * (np.abs(foot_pos[1]) + np.abs(foot_pos[4]))])
         xy_com_pos = np.exp(-np.sum(qpos[:2] - xy_target_pos) ** 2)
 
         # 2b. Vertical Position Component (robot should stand upright and maintain a certain height)
         z_target_pos = self.target_height
-        z_com_pos = 1. if z_target_pos - 0.1 < qpos[2] < z_target_pos + 0.1 \
-            else np.exp(-10 * (qpos[2] - z_target_pos) ** 2)
+        z_com_pos = np.exp(-(qpos[2] - z_target_pos) ** 4)
 
         r_com_pos = 0.5 * xy_com_pos + 0.5 * z_com_pos
 
         # 3. CoM Velocity Modulation
-        r_com_vel = np.exp(-np.linalg.norm(qvel[:3], 1) ** 2)
+        r_com_vel = np.exp(-multiplier * np.sum(qvel[:3]) ** 2)
 
         # 4. Foot Placement
         # 4a. Foot Alignment
         r_feet_align = np.exp(-multiplier * (foot_pos[0] - foot_pos[3]) ** 2)
 
         # 4b. Feet Width
-        target_width = 0.16
-        feet_width = np.linalg.norm([foot_pos[1], foot_pos[4]], 1)
-        r_foot_width = 1. if target_width - 0.01 < feet_width < target_width + 0.03 \
-            else np.exp(-multiplier * (feet_width - target_width) ** 2)
+        target_width = 0.2695434287408531
+        feet_width = np.abs(foot_pos[1]) + np.abs(foot_pos[4])
+        r_foot_width = np.exp(-np.sum(feet_width - target_width) ** 2)
 
-        # 4c. Foot Height
-        r_foot_height = np.exp(-multiplier * np.linalg.norm([foot_pos[2], foot_pos[5]]) ** 2)
-
-        r_foot_placement = 0.33 * r_feet_align + 0.33 * r_foot_width + 0.34 * r_foot_height
+        r_foot_placement = 0.5 * r_feet_align + 0.5 * r_foot_width
 
         # 5. Foot/Pelvis Orientation
         foot_yaw = np.array([qpos[8], qpos[22]])
-        left_foot_orient = np.exp(-multiplier * (foot_yaw[0] - qpos[6]) ** 2)
-        right_foot_orient = np.exp(-multiplier * (foot_yaw[1] - qpos[6]) ** 2)
+        left_foot_orient = np.exp(-(foot_yaw[0] - qpos[6]) ** 2)
+        right_foot_orient = np.exp(-(foot_yaw[1] - qpos[6]) ** 2)
 
         r_fp_orient = 0.5 * left_foot_orient + 0.5 * right_foot_orient
 
-        # TODO: 6.Leg Symmetry Reward
-        # r_symmetry = np.exp(-(qpos[7:21] - qpos[21:]) ** 2)
-
         # 7. Ground Force Modulation (Even Vertical Foot Force Distribution)
-        # TODO: How to incentivize to step rather than drag?
+        grf_tolerance = 50
+
         # GRF target discourages shear forces and incites even vertical foot force distribution
-        target_grf = (foot_grf[2] + foot_grf[5]) / 2.
-        left_grf = np.exp(-(np.linalg.norm(foot_grf[2] - target_grf) / grf_tolerance) ** 2)
-        right_grf = np.exp(-(np.linalg.norm(foot_grf[5] - target_grf) / grf_tolerance) ** 2)
+        target_grf = np.array([0., 0., np.sum(foot_grf) / 2.])
+        left_grf = np.exp(-(np.sum(foot_grf[:3] - target_grf) / grf_tolerance) ** 2)
+        right_grf = np.exp(-(np.sum(foot_grf[3:] - target_grf) / grf_tolerance) ** 2)
 
         # reward is only activated when both feet are down
-        r_grf = 0.5 * left_grf + 0.5 * right_grf
+        r_grf = 0.5 * left_grf + 0.5 * right_grf if foot_pos[2] < 2e-2 and foot_pos[5] < 2e-2 else 0.
 
-        # B. Target/Imitation Reward
+        # 8. Target Position Reward
         target_pos = np.array([0.0, 0.0, 1.01,
                                1.0, 0.0, 0.0, 0.0,
                                0.0045, 0.0, 0.4973,
@@ -244,6 +264,23 @@ class CassieEnv:
                                -1.1997, 0.0, 1.4267, 0.0, -1.5244, 1.5244, -1.5968])
         r_target_joint_pos = np.exp(-np.linalg.norm(qpos - target_pos) ** 2)
 
+        # Initial qpos for reference
+        # Pelvis Position
+        # 0.0, 0.0, 1.01,
+
+        # Pelvis Orientation
+        # 1.0, 0.0, 0.0, 0.0,
+
+        # Left Leg
+        # 0.0045, 0.0, 0.4973,
+        # 0.9784830934748516, -0.016399716640763992, 0.017869691242100763, -0.2048964597373501,
+        # -1.1997, 0.0, 1.4267, 0.0, -1.5244, 1.5244, -1.5968,
+
+        # Right Leg
+        # -0.0045, 0.0, 0.4973,
+        # 0.978614127766972, 0.0038600557257107214, -0.01524022001550036, -0.20510296096975877,
+        # -1.1997, 0.0, 1.4267, 0.0, -1.5244, 1.5244, -1.5968
+
         # Total Reward
         reward = (rw[0] * r_pose
                   + rw[1] * r_com_pos
@@ -253,7 +290,7 @@ class CassieEnv:
                   + rw[5] * r_grf
                   + rw[6] * r_target_joint_pos)
 
-        # ./train.py -r 0.3 --eps_steps 30 --training_steps 1e6 --save --tensorboard --tag "TargetPosition[]"
+        # print(foot_pos)
 
         print('Pose [{:.3f}], CoM [{:.3f}, {:.3f}], Foot [{:.3f}, {:.3f}], GRF[{:.3f}] Target [{:.3f}]'.format(r_pose,
                                                                                                                r_com_pos,
@@ -265,7 +302,7 @@ class CassieEnv:
 
         return reward
 
-    def compute_cost(self, qpos, foot_pos, foot_grf, cw=(0., 0., 0., 0.5)):
+    def compute_cost(self, qpos, foot_pos, foot_grf, cw=(0.3, 0.1, 0., 0.4)):
         # 1. Ground Contact (At least 1 foot must be on the ground)
         c_contact = 1. if (foot_grf[2] + foot_grf[5]) == 0. else 0.
 
@@ -302,19 +339,52 @@ class CassieEnv:
         # estimate power
         power_estimate = np.sum(motor_powers) + np.sum(power_losses)
 
-        power_threshold = 110  # Watts (Positive Work only)
+        power_threshold = 100  # Watts (Positive Work only)
         c_power = 1. / (1. + np.exp(-(power_estimate - power_threshold)))
 
-        # 3. Smooth Torques Cost
-        c_smooth_actions = 1 - np.exp(-np.sum(input_torques) ** 2)
+        # 3. Foot Dragging
+        left_drag_cost = 1. if foot_grf[2] > 0. and (np.abs(foot_grf[0]) + np.abs(foot_grf[1])) > 5 else 0.
+        right_drag_cost = 1. if foot_grf[5] > 0. and (np.abs(foot_grf[0]) + np.abs(foot_grf[1])) > 5 else 0.
+
+        c_foot_drag = 0.5 * left_drag_cost + 0.5 * right_drag_cost
 
         # 4. Falling
         c_fall = 1 if qpos[2] < self.min_height else 0
 
         # Total Cost
-        cost = cw[0] * c_contact + cw[1] * c_power + cw[2] * c_smooth_actions + cw[3] * c_fall
+        cost = cw[0] * c_contact + cw[1] * c_power + cw[2] * c_foot_drag + cw[3] * c_fall
 
         return cost
+
+    def get_ref_state(self, phase=None):
+        if phase is None:
+            phase = self.phase
+
+        if phase > self.phaselen:
+            phase = 0
+
+        pos = np.copy(self.trajectory.qpos[phase * self.simrate])
+
+        # this is just setting the x to where it "should" be given the number
+        # of cycles
+        # pos[0] += (self.trajectory.qpos[-1, 0] - self.trajectory.qpos[0, 0]) * self.counter
+
+        # ^ should only matter for COM error calculation,
+        # gets dropped out of state variable for input reasons
+
+        ###### Setting variable speed  #########
+        pos[0] *= self.speed
+        pos[0] += (self.trajectory.qpos[-1, 0] - self.trajectory.qpos[0, 0]) * self.counter * self.speed
+        ######                          ########
+
+        # setting lateral distance target to 0?
+        # regardless of reference trajectory?
+        pos[1] = 0
+
+        vel = np.copy(self.trajectory.qvel[phase * self.simrate])
+        vel[0] *= self.speed
+
+        return pos, vel
 
     def get_full_state(self):
 
