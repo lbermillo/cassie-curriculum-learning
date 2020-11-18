adcc24146c76a37e79f7b83357c775970bc2966b
lbermillo <lorenzo.bermillo@gmail.com>
2020-11-18 13:24:32 -0800

    Added Tanh output back since it wasn't able to learn w/out it

--- rl/networks/Actor.py
+++ rl/networks/Actor.py
@@ -26,6 +26,7 @@ class Actor(nn.Module):
         )
         self.l3 = nn.Sequential(
             nn.Linear(hidden_layer[1], action_dim),
+            nn.Tanh()
         )
 
         if init_weights:
